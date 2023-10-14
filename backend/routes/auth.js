50853e82f9a8fc76dbebea226486f34219de6ac1
Sahil Khan <kingkhan120702@gmail.com>
2023-10-14 15:51:47 +0530

    update project

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,97 +1,110 @@
-const express = require('express');
-const router = express.Router();
-const { body, validationResult } = require('express-validator');
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
-const fetchuser = require('../middleware/fetchuser')
-const jwt = require('jsonwebtoken')
-const JWT_auth = 'Sahilkhan$191202'
+const express = require("express");
+const routes = express.Router();
+const { body, validationResult } = require("express-validator");
+const User = require("../models/User");
+const fetchuser = require("../middleware/fetchuser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwt_sec = "Aniskhan1234";
 
-router.post('/createuser', [
-    body('email').isEmail(),
-    body('name').isLength({ min: 5 }),
-    body('password').isLength({ min: 5 }),
-], async (req, res) => {
+routes.post(
+  "/createuser",
+  [
+    body("email", "Enter valid Email").isEmail(),
+    body("name", "Enter Valid Name").isLength({ min: 5 }),
+    body("password", "Enter valid password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
     let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({success, errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
     try {
-        let user = await User.findOne({ email: req.body.email })
-        if (user) {
-            success = false;
-            return res.status(400).json({ success,errors: "Please enter valid email the email is already registered" });
-        }
-        const salt = await bcrypt.genSalt(10);
-        const secPass = await bcrypt.hash(req.body.password, salt);
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ success, errors: "Sorry a  user With email already Exist" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
 
-        user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: secPass,
-        });
-        const data = {
-            user: {
-                id: user.id
-            }
-        }
-        const authToken = jwt.sign(data, JWT_auth);
-        success = true;
-        res.json({ success,authToken })
+      //   .then(user => res.json(user))
+      //   .catch(err=> {
+      // res.json({error : "Please Enter Valid Input" , message :err.message})})
+      const data = {
+        user: { id: user.id },
+      };
+      const token = jwt.sign(data, jwt_sec);
+      success = true;
+      res.json({ success, token });
     } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Some Error Occured")
+      console.log(error.message);
+      res.status(5000).send("Some Error occured");
     }
-});
+  }
+);
 
-router.post('/login', [
-    body('email').isEmail(),
-    body('password').exists({ min: 5 }),
-], async (req, res) => {
+// login
+
+routes.post(
+  "/login",
+  [
+    body("email", "Enter valid Email").isEmail(),
+    body("password", "Enter valid password").exists(),
+  ],
+  async (req, res) => {
     let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
     const { email, password } = req.body;
     try {
-        let user = await User.findOne({ email });
-        if (!user) {
-            success = false;
-            return res.status(400).json({  success,error: "Account Does Not Exists" })
-        }
-        const passwordCompare = await bcrypt.compare(password, user.password);
-        if (!passwordCompare) {
-            success = false;
-            return res.status(400).json({ success ,error: "Password Entered Is Incorrect" })
-        }
-        const data = {
-            user: {
-                id: user.id
-            }
-        }
-        const authToken = jwt.sign(data, JWT_auth);
-        success = true;
-        res.json({ success,authToken })
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ success, errors: "Please enter valid  credentails" });
+      }
+      const compare = await bcrypt.compare(password, user.password);
+      if (!compare) {
+        return res
+          .status(400)
+          .json({ success, errors: "Please enter valid  credentails" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = await jwt.sign(data, jwt_sec);
+      success = true;
+      res.json({ success, token });
     } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Some Error Occured")
-
+      success = false;
+      console.log(error.message);
+      res.status(5000).send(success, "Some Error occured");
     }
-});
-router.post('/getuser',fetchuser, async (req, res) => {
-    try {
-        userid = req.user.id;
-        const user = await User.findById(userid).select("-password")
-        res.send(user)
-        
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Some Error Occured")
-        
-    }
+  }
+);
 
-});
+// getuser
 
-module.exports = router;
+routes.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    const userID = await req.user.id;
+    const user = await User.findById(userID).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(5000).send("Some Error occured");
+  }
+});
+module.exports = routes;
